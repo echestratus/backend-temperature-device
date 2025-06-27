7db4541a025ed87261568418d6e629aa4b5174dd
echestratus <farhanz.nh.13@gmail.com>
2025-06-27 15:14:10 +0700

    feat(db): Change the db from local to the one hosted on aiven

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,13 +1,19 @@
 const pg = require('pg');
 const { Pool } = pg
 require('dotenv').config();
+
+const caCert = process.env.DB_SSL_CA.replace(/\\n/g, '\n');
  
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: true, // Enable full verification
+    ca: caCert
+  }
 });
 
 module.exports = {
