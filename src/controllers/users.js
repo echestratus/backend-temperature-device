5b8d200e808509e65f3141cd06bc3a992947fc9e
root <root@srv888247.hstgr.cloud>
2025-07-02 01:18:45 +0000

    fix(users): Change secure for token cookie to none

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -181,7 +181,7 @@ const loginUser = async (req, res, next) => {
         res.cookie('token', token, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000,
-            secure: true,
+            secure: false,
             sameSite: 'lax'
         });
         return response(res, "success", 200, "Login Succeed", responseData);
